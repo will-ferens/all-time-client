53b1ea1a7afdf657bcfd871bc49e9a5c5ad36fcd
_dream <will.ferens@gmail.com>
2021-07-06 15:23:41 -0600

    removed console log

--- src/api/spotify.js
+++ src/api/spotify.js
@@ -2,8 +2,6 @@ const authEndpoint = "https://accounts.spotify.com/authorize";
 const redirectUri = process.env.REACT_APP_REDIRECT_URI;
 export const clientId = "4cf4914b17c340e68221b922b1d20336";
 
-console.log(process.env.REACT_APP_REDIRECT_URI);
-
 const scopes = ["user-top-read", "user-read-email", "user-read-private"];
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scopes.join(
